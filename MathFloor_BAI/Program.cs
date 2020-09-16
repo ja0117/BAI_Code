13a07390392378c48928c905bdae508a841dc99d
Joshua <joshua007.joshua@gmail.com>
2020-09-16 13:50:31 +0200

    Add project files.

---
+++ MathFloor_BAI/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week1_BuzzGame
+{
+    public class Program
+    {
+        public string[] Players = { "Player A", "Player B", "Player C", "Player D", "Player E", "Player F", "Player G" };
+        public int[] PlayerScores = new int[7];
+        public int CurrentPlayer = 0;
+        public int CurrentNumber = 0;
+        public int MaxNumber = 1000;
+        public int Buzz = 0;
+        public int NoBuzz = 0;
+        public int digit = 7;
+        public static void Main(string[] args) 
+        {
+            Program run = new Program();
+            run.RunGame();
+        }
+
+        public void RunGame()
+        {
+            while (CurrentNumber < MaxNumber)
+            {
+                for (int i = 0; i < Players.Length; i++)
+                {
+                    CurrentNumber = CurrentNumber + 1;
+                    Console.WriteLine($"{Players[i]} said: {CurrentNumber}");
+                    if (CurrentNumber.ToString().Contains(digit.ToString()) || isMultiple(CurrentNumber, digit))
+                    {
+                        PlayerScores[i] = PlayerScores[i] + 1;
+                        Buzz = Buzz + 1;
+                    }
+                    else
+                    {
+                        NoBuzz = NoBuzz + 1;
+                    }
+                }
+            }
+            ShowResults();
+        }
+
+        public void ShowResults()
+        {
+            for (int i = 0; i < Players.Length; i++)
+            {
+                Console.WriteLine($"{Players[i]} has Buzzed {PlayerScores[i]} times");
+            }
+            Console.ReadLine();
+        }
+
+        public bool isMultiple(int a, int b)
+        {
+            if (a % b == 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ week2_integerArray60/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week2_integerArray60
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Oefening2();
+        }
+        public void Oefening1()
+        {
+            const int AANTAL = 60;
+            const int MAX = 100;
+            int[] getallen = new int[AANTAL];
+            Random random = new Random();
+
+            for (int i = 0; i < AANTAL; i++)
+            {
+                getallen[i] = random.Next(MAX);
+            }
+
+            int GrootsteIndex = 0;
+            // Get Biggest index 
+            for (int i = 1; i < AANTAL; i++)
+            {
+                if (getallen[i] > getallen[GrootsteIndex])
+                {
+                    GrootsteIndex = i;
+                }
+            }
+            Console.WriteLine($"Het grootste getal is {getallen[GrootsteIndex]} en staat op plek {GrootsteIndex}");
+            foreach (int getal in getallen)
+            {
+                Console.WriteLine($"Random Getal: {getal}");
+            }
+            Console.ReadLine();
+        }
+
+        public static void Oefening2() {
+            const int AANTAL = 10;
+            const int MAX = 60;
+            int[] getallen = new int[AANTAL];
+            Random random = new Random();
+
+            for (int i = 0; i < AANTAL; i++)
+            {
+                getallen[i] = random.Next(MAX);
+            }
+
+            int kleinste, kleinsteAantal;
+            int eenNaKleinste, eenNaKleinsteAantal;
+
+            kleinsteAantal = eenNaKleinsteAantal = 0;
+            kleinste = eenNaKleinste = MAX;
+
+            // Get Index with smallest number and second to smallest number  n
+            foreach (int getal in getallen)
+            {
+                if (getal < kleinste)
+                {
+                    eenNaKleinste = kleinste;
+                    kleinste = getal;
+                }
+                else if (getal > kleinste && getal < eenNaKleinste)
+                {
+                    eenNaKleinste = getal;
+                }
+            }
+            Console.WriteLine($"Het kleinste getal {kleinste} ({kleinsteAantal} x)");
+            Console.WriteLine($"Het op 1 na kleinste getal is {eenNaKleinste} ({kleinsteAantal} x)");
+
+
+            foreach (int getal in getallen)
+            {
+                Console.WriteLine($"Random Getal: {getal}");
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
